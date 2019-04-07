164e931e159faf32aec37d5f5409122012176e1b
Edith Gómez <edith.gomez4@outlook.com>
2019-04-06 23:04:49 -0700

    actualización diseños de las actividades, se añade la actividad SelecImagen

---
+++ RESTAURANTES/app/src/main/java/edith/example/restaurantes/Mostrar.java
@@ -0,0 +1,13 @@
+package edith.example.restaurantes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Mostrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar);
+    }
+}
