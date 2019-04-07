d2a81a5ff2e8a1df8f850de76df3b19e781fe8cf
Pollo <danielquintanilla9024@gmail.com>
2019-04-06 17:52:12 -0700

    prueba de botones

--- RESTAURANTES/app/src/main/java/edith/example/restaurantes/Principal.java
+++ RESTAURANTES/app/src/main/java/edith/example/restaurantes/Principal.java
@@ -2,6 +2,8 @@ package edith.example.restaurantes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    public void botonazo(View view) {
+        switch (view.getId()){
+            case R.id.btnCap:
+                Toast.makeText(this,"captura",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnMos:
+                Toast.makeText(this,"mostrar",Toast.LENGTH_SHORT).show();
+            case R.id.btnSal:
+                Toast.makeText(this,"salir",Toast.LENGTH_SHORT).show();
+        }
     }
 }
